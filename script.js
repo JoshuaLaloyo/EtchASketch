ed57834ed3cdf2e43703adc7d788e4272762bb60
JoshuaLaloyo <joshualaloyo1@gmail.com>
2026-02-12 13:18:53 +0300

    done

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
-let body = document.querySelector(`body`);
-let  button = document.createElement(`button`);
-body.append(button);
-button.style.height = `2rem`;
-button.addEventListener(`click`, function(){
-    const squares = prompt(`input the number of squares you want:`);
-})
-button.textContent = `Click here to setup grid`;
-let contdiv = document.createElement(`div`);
-let div = document.createElement(`div`);
-body.appendChild(div);
-div.style.border = `solid blue 2px`
-div.style.height = `192px`;
-div.style.width = `192px`;
-div.style.display = `flex`;
-div.style.flexWrap = `wrap`;
-div.style.gap = `0px`;
-div.style.justifyContent = `flex-start`;
-for (let i=0; i<16*16; i++){
-let div2 = document.createElement(`div`);
-div.append(div2)
-div2.style.border = `solid 1px`;
-div2.style.height = `10px`;
-div2.style.width = `10px`;
-div2.style.margin = `0`;
-div2.style.padding = `0`;
-div2.addEventListener(`mouseover`, function(){
-    div2.style.backgroundColor = `blue`;
-});
-}
+
+for (i=0; i<16*16; i++) {
+    const grid = document.createElement(`div`)
+    const container = document.querySelector(`.container`)
+    container.appendChild(grid)
+    grid.style.border = 'solid 1px blue'
+    container.style.border = 'solid 1px black'
+    grid.style.width = '10px'
+    grid.style.height = '10px'
+    container.style.display = 'flex'
+    container.style.flexWrap = 'wrap'
+    container.style.height = '192px'
+    container.style.width = '192px'
+    container.style.margin = '0 auto'
+    grid.addEventListener('mouseover', write)
+    }
+
+    const reset = document.createElement(`button`)
+    const body = document.querySelector(`body`)
+    body.appendChild(reset)
+    reset.textContent = 'Reset'
+    const input = document.createElement(`button`)
+    const h1 = document.querySelector(`h1`)
+    h1.append(input)
+    input.textContent = 'Change Grid Size'
+
+    input.addEventListener('click', () => {
+        const newSize = prompt('Enter new grid size (e.g. 16 for 16x16)')
+        const container = document.querySelector(`.container`)
+        container.innerHTML = ''
+        if (newSize < 1 || newSize >= 100) {
+            alert('Please enter a number between 1 and 100')
+            return
+        }
+        for (i=0; i<newSize*newSize; i++) {
+            const grid = document.createElement(`div`)
+            container.appendChild(grid)
+            container.style.border = 'solid 1px black'
+            container.style.display = 'flex'
+            container.style.flexWrap = 'wrap'
+            container.style.height = `192px`
+            container.style.width = `192px`
+            grid.style.border = 'solid 1px blue'
+            grid.style.margin = '0'
+            grid.style.width = `${(192/newSize)-2}px`
+            grid.style.height = `${(192/newSize)-2}px`
+            grid.addEventListener('mouseover', write)
+        }
+    })
+
+    function resetGrid() {        
+    const grid = document.querySelectorAll(`.container div`)
+        grid.forEach(div => div.style.backgroundColor = 'white')
+    }
+
+    reset.addEventListener('click', resetGrid)
+    function write(e) {
+        e.target.style.backgroundColor = 'blue'
+    }
+ 
